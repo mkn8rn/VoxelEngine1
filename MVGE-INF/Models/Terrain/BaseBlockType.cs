e6d99512e93a1994fcde14f09ee44f282c424c24
Markinatorina <markinatorina@hotmail.com>
2025-09-12 09:40:35 +0200

    bbtypes

--- MVGE-INF/Models/Terrain/BaseBlockType.cs
+++ MVGE-INF/Models/Terrain/BaseBlockType.cs
@@ -10,14 +10,14 @@ namespace MVGE_INF.Models.Terrain
     {
         Empty = 0,
         Gas = 1,
-        // empty = 2
+        Mist = 2,
         Mineral = 3,
-        // empty = 4
+        Metal = 4,
         Soil = 5,
         Stone = 6,
         Wood = 7,
         Fungus = 8,
-        // empty = 9
+        Flesh = 9,
         Glass = 10,
         Water = 11,
     }
